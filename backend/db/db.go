9099a853765a6ea1d2fe70996b8ef60993d656ee
Aser <aserdipanda@gmail.com>
2021-04-21 21:25:18 +0100

    Project setup

---
+++ backend/db/db.go
@@ -0,0 +1,6 @@
+package db
+
+//Init start the connection to the database
+func Init() {
+
+}
--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/osscameroon/yotas/backend/app"
+	"log"
+	"net/http"
+)
 
 func main() {
 
-	fmt.Println("YOTAS")
+	_ = godotenv.Load()
+
+	app.InitEnv()
+
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+
+	// ─── API ROUTER ────────────────────────────────────────────────────
+	apiRouter := router.Group("/api")
+
+	apiRouterV1 := apiRouter.Group("/v1")
+	apiRouterV1.GET("", func(c *gin.Context) {
+		c.String(http.StatusOK, "Yotas")
+	})
+
+	// ─── WEB APP ───────────────────────────────────────────────────────
+	router.Use(static.Serve("/", static.LocalFile("./frontend", true)))
+
+	log.Println("HTTP Server Started on port ", app.Env.HttpPort)
+	err := router.Run(":" + app.Env.HttpPort)
+	log.Fatal(err)
 }
