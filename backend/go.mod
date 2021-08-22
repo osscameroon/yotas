961af9496551d2e895b304367e75b4614cf58ab8
sanix-darker <s4nixd@gmail.com>
2021-08-22 18:10:57 +0200

    fix(migrations): psql fix synthaxes

--- backend/go.mod
+++ backend/go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.7.1
 	github.com/joho/godotenv v1.3.0
+	github.com/pressly/goose/v3 v3.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	gorm.io/driver/postgres v1.1.0
 	gorm.io/gorm v1.21.10
 )
---
+++ backend/migrations/002_create_tables.sql
@@ -0,0 +1,204 @@
+-- +goose Up
+
+-- +goose StatementBegin
+CREATE SEQUENCE IF NOT EXISTS organisations_seq;
+CREATE TABLE IF NOT EXISTS organisations (
+    id INTEGER NOT NULL DEFAULT NEXTVAL ('organisations_seq'),
+
+    name VARCHAR(150),
+    email VARCHAR(150) NOT NULL UNIQUE,
+    github_id VARCHAR(150) NOT NULL UNIQUE,
+    avatar_url VARCHAR(300),
+    web_site VARCHAR(300),
+    description VARCHAR(300),
+
+    created_at TIMESTAMP(0),
+    updated_at TIMESTAMP(0),
+    deleted_at TIMESTAMP(0),
+
+    PRIMARY KEY (id)
+);
+-- +goose StatementEnd
+
+-- +goose StatementBegin
+CREATE SEQUENCE IF NOT EXISTS users_seq;
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER NOT NULL DEFAULT NEXTVAL ('users_seq'),
+
+    name VARCHAR(150),
+    email VARCHAR(150) NOT NULL UNIQUE,
+    github_id VARCHAR(150) NOT NULL UNIQUE,
+    github_token VARCHAR(300) NOT NULL,
+    avatar_url VARCHAR(300),
+
+    created_at TIMESTAMP(0),
+    updated_at TIMESTAMP(0),
+    deleted_at TIMESTAMP(0),
+
+    PRIMARY KEY(id)
+);
+-- +goose StatementEnd
+
+
+-- +goose StatementBegin
+CREATE SEQUENCE IF NOT EXISTS wallets_seq;
+CREATE TABLE IF NOT EXISTS wallets (
+    id INTEGER NOT NULL DEFAULT NEXTVAL ('wallets_seq'),
+
+    wallet_id VARCHAR(300) UNIQUE NOT NULL,
+    user_id   INTEGER NOT NULL,
+    organisation_id   INTEGER NOT NULL,
+    balance INTEGER NOT NULL,
+
+    created_at TIMESTAMP(0),
+    updated_at TIMESTAMP(0),
+    deleted_at TIMESTAMP(0),
+
+    PRIMARY KEY(id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (organisation_id) REFERENCES organisations (id)
+);
+-- +goose StatementEnd
+
+
+-- +goose StatementBegin
+CREATE SEQUENCE IF NOT EXISTS articles_seq;
+CREATE TABLE IF NOT EXISTS articles (
+    id INTEGER NOT NULL DEFAULT NEXTVAL ('articles_seq'),
+
+    name VARCHAR(150),
+    description TEXT,
+    quantity INTEGER,
+    price INTEGER NOT NULL,
+    metadata TEXT,
+
+    created_at TIMESTAMP(0),
+    updated_at TIMESTAMP(0),
+    deleted_at TIMESTAMP(0),
+
+    PRIMARY KEY(id)
+);
+-- +goose StatementEnd
+
+
+-- +goose StatementBegin
+CREATE SEQUENCE IF NOT EXISTS orders_seq;
+CREATE TABLE IF NOT EXISTS orders (
+    id INTEGER NOT NULL DEFAULT NEXTVAL ('orders_seq'),
+
+    wallet_id VARCHAR(300) NOT NULL,
+    article_id INTEGER NOT NULL,
+    quantity INTEGER,
+
+    created_at TIMESTAMP(0),
+    updated_at TIMESTAMP(0),
+    deleted_at TIMESTAMP(0),
+
+    PRIMARY KEY(id),
+    FOREIGN KEY (wallet_id) REFERENCES wallets(wallet_id),
+    FOREIGN KEY (article_id) REFERENCES articles(id)
+);
+-- +goose StatementEnd
+
+
+-- +goose StatementBegin
+CREATE SEQUENCE IF NOT EXISTS organisations_articles_seq;
+CREATE TABLE IF NOT EXISTS organisations_articles (
+    id INTEGER NOT NULL DEFAULT NEXTVAL ('organisations_articles_seq'),
+
+    organisation_id INTEGER NOT NULL,
+    article_id INTEGER NOT NULL,
+
+    created_at TIMESTAMP(0),
+    updated_at TIMESTAMP(0),
+    deleted_at TIMESTAMP(0),
+
+    PRIMARY KEY(id),
+    FOREIGN KEY (organisation_id) REFERENCES organisations (id),
+    FOREIGN KEY (article_id) REFERENCES articles (id)
+);
+-- +goose StatementEnd
+
+
+-- +goose StatementBegin
+CREATE SEQUENCE IF NOT EXISTS organisations_users_seq;
+CREATE TABLE IF NOT EXISTS organisations_users (
+    id INTEGER NOT NULL DEFAULT NEXTVAL ('organisations_users_seq'),
+
+    organisation_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    active BOOLEAN,
+
+    created_at TIMESTAMP(0),
+    updated_at TIMESTAMP(0),
+    deleted_at TIMESTAMP(0),
+
+    PRIMARY KEY(id),
+    FOREIGN KEY (organisation_id) REFERENCES organisations (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+-- +goose StatementEnd
+
+
+-- +goose StatementBegin
+CREATE SEQUENCE IF NOT EXISTS operations_seq;
+CREATE TYPE op_type AS ENUM ('buy','receive');
+CREATE TABLE IF NOT EXISTS operations (
+    id INTEGER NOT NULL DEFAULT NEXTVAL ('operations_seq'),
+
+    amount INTEGER NOT NULL,
+    wallet_id VARCHAR(300),
+    operation_type op_type,
+    approved       BOOLEAN,
+    operation_hash TEXT UNIQUE,
+
+    created_at TIMESTAMP(0),
+    updated_at TIMESTAMP(0),
+    deleted_at TIMESTAMP(0),
+
+    PRIMARY KEY(id),
+    FOREIGN KEY (wallet_id) REFERENCES wallets(wallet_id)
+);
+-- +goose StatementEnd
+
+
+-- +goose StatementBegin
+CREATE SEQUENCE IF NOT EXISTS pictures_seq;
+CREATE TABLE IF NOT EXISTS pictures (
+    id  INTEGER NOT NULL DEFAULT NEXTVAL ('pictures_seq'),
+
+    organisation_id INTEGER NOT NULL,
+
+    alt_text TEXT,
+    original TEXT,
+    small TEXT,
+    medium TEXT,
+    large TEXT,
+
+    created_at TIMESTAMP(0),
+    updated_at TIMESTAMP(0),
+    deleted_at TIMESTAMP(0),
+
+    PRIMARY KEY(id),
+    FOREIGN KEY (organisation_id) REFERENCES organisations(id)
+);
+-- +goose StatementEnd
+
+
+-- +goose StatementBegin
+CREATE SEQUENCE IF NOT EXISTS articles_pictures_seq;
+CREATE TABLE IF NOT EXISTS articles_pictures(
+    id INTEGER NOT NULL DEFAULT NEXTVAL ('articles_pictures_seq'),
+
+    picture_id INTEGER NOT NULL,
+    article_id INTEGER NOT NULL,
+    created_at TIMESTAMP(0),
+    updated_at TIMESTAMP(0),
+    deleted_at TIMESTAMP(0),
+
+    PRIMARY KEY(id),
+    FOREIGN KEY (article_id) REFERENCES articles(id),
+    FOREIGN KEY (picture_id) REFERENCES pictures(id)
+);
+-- +goose StatementEnd
+-- +goose Down
