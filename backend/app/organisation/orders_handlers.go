d83c9ecca09d7feee9979450e086aa334b912a92
Aser <aserdipanda@gmail.com>
2021-08-27 20:39:31 +0100

    feat(backend): add GetOrderHandler for single order

--- backend/app/organisation/orders_handlers.go
+++ backend/app/organisation/orders_handlers.go
@@ -53,7 +53,7 @@ func GetOrganisationOrdersHandler(ctx *gin.Context) {
 			return
 		}
 
-		articlesID := make([]uint, len(orders))
+		articlesID := make([]uint, len(orderArticles))
 		for orderArticleIndex := 0; orderArticleIndex < len(orderArticles); orderArticleIndex++ {
 			articlesID = append(articlesID, orderArticles[orderArticleIndex].ArticleID)
 		}
@@ -92,6 +92,75 @@ func GetOrganisationOrdersHandler(ctx *gin.Context) {
 		"offset": offset,
 		"data":   ordersPresenter,
 	})
+}
+
+func GetUserOrdersHandler(ctx *gin.Context) {
+
+	// TODO Retreive user information an user the same flow in GetOrganisationOrdersHandler
+}
+
+func GetOrderHandler(ctx *gin.Context) {
+
+	/*	organisationID, err := strconv.Atoi(ctx.GetHeader("Tenant"))
+		if err != nil {
+			ctx.String(http.StatusBadRequest, ErrTenantNotProvided.Error())
+			return
+		}*/
+
+	orderID, err := strconv.Atoi(ctx.Param("orderID"))
+	if err != nil {
+		ctx.String(http.StatusBadRequest, "Order id must be an int")
+		return
+	}
+
+	// TODO check if the issuer of this request is the owner of the order or a manager of the organisation
+
+	order, err := GetOrder(uint(orderID))
+	if err != nil {
+		return
+	}
+	orderPresenter := OrderPresenter{}
+	orderPresenter.Orders = order
+
+	orderArticles, err := GetOrderArticles(order.ID)
+	if err != nil {
+		return
+	}
+
+	articlesID := make([]uint, len(orderArticles))
+	for orderArticleIndex := 0; orderArticleIndex < len(orderArticles); orderArticleIndex++ {
+		articlesID = append(articlesID, orderArticles[orderArticleIndex].ArticleID)
+	}
+
+	articles, err := GetArticles(articlesID)
+	if err != nil {
+		return
+	}
+
+	for orderArticleIndex := 0; orderArticleIndex < len(orderArticles); orderArticleIndex++ {
+		// search for articles
+		article := Articles{}
+		for articleIndex := 0; articleIndex < len(articles); articleIndex++ {
+			if articles[articleIndex].ID == orderArticles[orderArticleIndex].ArticleID {
+				article = articles[articleIndex]
+				break
+			}
+		}
+
+		pictures, err := GetArticlePictures(article.ID)
+		if err != nil {
+			return
+		}
+		orderPresenter.Items = append(orderPresenter.Items, &OrderItemPresenter{
+			OrdersArticles: &orderArticles[orderArticleIndex],
+			Article: ArticlesPresenter{
+				Articles: article,
+				Pictures: pictures,
+			},
+		})
+	}
+
+	ctx.JSON(http.StatusOK, orderPresenter)
 }
 
 func CreateOrderHandler(ctx *gin.Context) {
@@ -138,10 +207,6 @@ func CreateOrderHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, order)
 }
 
-func GetUserOrdersHandler(ctx *gin.Context) {
-
-}
-
-func GetOrderHandler(ctx *gin.Context) {
+func UpdateOrder(ctx *gin.Context) {
 
 }
