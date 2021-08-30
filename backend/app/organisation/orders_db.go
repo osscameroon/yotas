272112da1d94cc5ddd666e10bd0d5462c5080e96
Aser <aserdipanda@gmail.com>
2021-08-31 00:08:05 +0100

    feat(backend): update - CreateOrder - DeclineOrder - AcceptOrder - PayOrder

--- backend/app/organisation/orders_db.go
+++ backend/app/organisation/orders_db.go
@@ -9,28 +9,53 @@ import (
 )
 
 type OrderState string
+type OperationType string
 
 const (
+	orderStateNotPaid  OrderState = "not paid"
 	orderStateToReview OrderState = "to review"
 	orderStateAccepted OrderState = "accepted"
 	orderStateDeclined OrderState = "declined"
 )
 
+const (
+	operationTypeRefund OperationType = "refund"
+	operationTypeCredit OperationType = "credit"
+	operationTypeDebit  OperationType = "debit"
+)
+
 func CreateOrder(order *Orders, orderItems []*OrderItemPresenter) error {
 	err := db.Session.Transaction(func(tx *gorm.DB) error {
 
-		order.State = string(orderStateToReview)
+		// We first create the order
+		order.State = string(orderStateNotPaid)
+		order.CreatedAt = time.Now().UTC()
 		err := tx.Create(order).Error
 		if err != nil {
-			return errors.New("can't create orders")
+			return errors.New("can't create order")
 		}
 
+		var totalOrderAmount int64
+		// we create each order OrdersArticles and summing the order amount
 		for _, orderArticle := range orderItems {
+			//sum the amount of article
+			totalOrderAmount += int64(orderArticle.Quantity) * orderArticle.Article.Price
+
 			orderArticle.OrderID = order.ID
+			orderArticle.ArticlePrice = orderArticle.Article.Price
+			orderArticle.CreatedAt = time.Now().UTC()
 			err = tx.Create(orderArticle.OrdersArticles).Error
 			if err != nil {
 				return fmt.Errorf("can't create orders process fail when creating order item %s", orderArticle.Article.Name)
 			}
+		}
+
+		// finally, we update the order amount
+		order.TotalAmount = totalOrderAmount
+		order.UpdatedAt = time.Now().UTC()
+		err = tx.Save(order).Error
+		if err != nil {
+			return errors.New("can't create order")
 		}
 
 		return nil
@@ -80,16 +105,134 @@ func GetOrderArticles(orderID uint) ([]OrdersArticles, error) {
 	return results, err
 }
 
-func DeclineOrder(orderID uint, state OrderState) {
+func DeclineOrder(orderID uint, reason string) error {
 
+	err := db.Session.Transaction(func(tx *gorm.DB) error {
+
+		var order Orders
+		err := tx.Model(&Orders{}).Where("id = ?", orderID).First(&order).Error
+		if err != nil {
+			return errors.New("can't find order")
+		}
+
+		order.State = string(orderStateDeclined)
+		order.Decision = reason
+		order.UpdatedAt = time.Now().UTC()
+
+		err = tx.Save(&order).Error
+		if err != nil {
+			return errors.New("can't update order state")
+		}
+
+		//TODO implement operation hash
+		curTime, _ := time.Now().UTC().MarshalText()
+		err = tx.Create(&Operations{
+			Model:         db.Model{CreatedAt: time.Now().UTC()},
+			Amount:        order.TotalAmount,
+			WalletId:      order.WalletId,
+			OperationType: string(operationTypeRefund),
+			Approved:      true,
+			OperationHash: string(curTime),
+		}).Error
+
+		if err != nil {
+			return errors.New("can't create refund operation")
+		}
+
+		var wallet Wallets
+		err = tx.Model(&Wallets{}).Where("wallet_id = ?", order.WalletId).First(&wallet).Error
+		if err != nil {
+			return errors.New("can't find user wallet")
+		}
+
+		wallet.Balance += order.TotalAmount
+		wallet.UpdatedAt = time.Now().UTC()
+		err = tx.Save(&wallet).Error
+		if err != nil {
+			return errors.New("can't refund wallet")
+		}
+
+		return nil
+	})
+
+	return err
 }
 
-func AcceptOrder() {
+func AcceptOrder(orderID uint, reason string) error {
+	err := db.Session.Transaction(func(tx *gorm.DB) error {
+		var order Orders
+		err := tx.Model(&Orders{}).Where("id = ?", orderID).First(&order).Error
+		if err != nil {
+			return errors.New("can't find order")
+		}
 
+		order.State = string(orderStateAccepted)
+		order.Decision = reason
+		order.UpdatedAt = time.Now().UTC()
+
+		err = tx.Save(&order).Error
+		if err != nil {
+			return errors.New("can't update order state")
+		}
+		return nil
+	})
+
+	return err
 }
 
-func PayOrder(orderID uint) {
+func PayOrder(orderID uint) error {
+	err := db.Session.Transaction(func(tx *gorm.DB) error {
 
+		var order Orders
+		err := tx.Model(&Orders{}).Where("id = ?", orderID).First(&order).Error
+		if err != nil {
+			return errors.New("can't find order")
+		}
+
+		// check if the wallet has enough yotas to pay the order
+		var wallet Wallets
+		err = tx.Model(&Wallets{}).Where("wallet_id = ?", order.WalletId).First(&wallet).Error
+		if err != nil {
+			return errors.New("can't find user wallet")
+		}
+
+		if wallet.Balance < order.TotalAmount {
+			return errors.New("wallet doesn't have enough money")
+		}
+
+		order.State = string(orderStateToReview)
+		order.UpdatedAt = time.Now().UTC()
+		err = tx.Save(&order).Error
+		if err != nil {
+			return errors.New("can't update order state")
+		}
+
+		//TODO implement operation hash
+		curTime, _ := time.Now().UTC().MarshalText()
+		err = tx.Create(&Operations{
+			Model:         db.Model{CreatedAt: time.Now().UTC()},
+			Amount:        order.TotalAmount,
+			WalletId:      order.WalletId,
+			OperationType: string(operationTypeDebit),
+			Approved:      true,
+			OperationHash: string(curTime),
+		}).Error
+
+		if err != nil {
+			return errors.New("can't create refund operation")
+		}
+
+		wallet.Balance -= order.TotalAmount
+		wallet.UpdatedAt = time.Now().UTC()
+		err = tx.Save(&wallet).Error
+		if err != nil {
+			return errors.New("can't refund wallet")
+		}
+
+		return nil
+	})
+
+	return err
 }
 
 func DeleteOrder(orderID uint) error {
