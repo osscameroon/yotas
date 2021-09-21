ce00ae186e2370b9ee5d6bc938c3fbd7dbd4043c
elhmn <nleme@live.fr>
2021-09-21 18:08:52 +0200

    feat: remove sudo command from psql install

--- backend/dev/setup-postgres/setup-postgres.sh
+++ backend/dev/setup-postgres/setup-postgres.sh
@@ -15,7 +15,7 @@ fi
 #install psql
 if [[ -z "$(which psql)" ]]; then
 	sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-	wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+	wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 	apt-get update
 	apt-get -y install postgresql
 fi
